9015ecae729647601c516f7b43ffc7eb17d56326
Simone Vittori <hello@simonewebdesign.it>
2018-07-19 19:29:32 +0100

    fps control

--- js/main.js
+++ js/main.js
@@ -3,32 +3,55 @@ const render = () => {}
 
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
-ctx.fillStyle = 'green'
-ctx.fillRect(10, 10, 100, 100)
-
-
-// The main game loop
-const main = () => {
-    const now = Date.now()
-    const delta = now - then
-
-    update(delta / 1000)
-    render()
-
-    then = now
-
-    // Request to do this again ASAP
-    requestAnimationFrame(main)
-}
 
 // Cross-browser support for requestAnimationFrame
 const win = window,
-    requestAnimationFrame =
+    raf =
          win.requestAnimationFrame
       || win.webkitRequestAnimationFrame
-      || win.msRequestAnimationFrame
       || win.mozRequestAnimationFrame
+      || win.msRequestAnimationFrame
+      || win.oRequestAnimationFrame
+
+const fpsRangeInput = document.getElementById('fps')
+const getFps = () => parseInt(fpsRangeInput.value)
+
+let framesCount = 0
 
 let then = Date.now()
+
+let x = 0,
+    y = 0
+
+// The main game loop
+const main = () => {
+    raf(main)
+
+    const now = Date.now()
+    const delta = (now - then) / 1000
+    const fps = getFps()
+    const interval = 1 / fps
+
+    if (delta > interval) {
+        framesCount++
+
+        ctx.clearRect(0, 0, 300, 150) // clear screen
+
+        ctx.fillStyle = 'green'
+        ctx.fillRect(x++, y++, 20, 20)
+
+        ctx.fillStyle = 'Black'
+        ctx.font      = 'normal 16pt Arial'
+        ctx.fillText(`FPS: ${fps} #: ${framesCount}`, 10, 26)
+
+        update(delta)
+        render()
+
+        // Update time
+        // now - (delta % interval) is an improvement over just
+        // using then = now, which can end up lowering overall fps
+        then = now - (delta % interval)
+    }
+}
 
 main()
